92ca2bbf12b758fe8cae0e0a26119b5e80bedc15
James Philhower <jamesphilhower@gmail.com>
2023-10-08 17:37:28 -0400

    updated deps

--- blogr-nextjs-prisma/pages/_app.tsx
+++ blogr-nextjs-prisma/pages/_app.tsx
@@ -1,14 +1,19 @@
-import { SessionProvider } from 'next-auth/react';
-import { AppProps } from 'next/app';
-import React, {useEffect} from 'react';
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-
-const App = ({ Component, pageProps }: AppProps) => {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-};
+}
 
-export default App;
+export default MyApp;
